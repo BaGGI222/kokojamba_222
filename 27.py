b74f1469d0bc5cdd78260887d2239c88d35e6fba
BodyaQ <vgretchenko77@gmail.com>
2023-07-11 22:12:30 +0300

    test1

--- 27.py
+++ 27.py
@@ -1,1 +1,9 @@
-print("Hello")
+def palindrome(s): 
+    return s[::-1] == s 
+
+while True: 
+    s = input("введите слово: ") 
+    if palindrome(s):
+        print(f"{s} это слово является палиндромом")  
+    else:
+        print("это слово не является палиндромом")
