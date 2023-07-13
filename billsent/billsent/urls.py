b5185b6408ccd28e41ca09db3d931d8a6840fc65
BodyaQ <vgretchenko77@gmail.com>
2023-07-13 18:50:08 +0300

    test1

--- billsent/billsent/urls.py
+++ billsent/billsent/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('app_billsent.urls')),
-    path('', include('app_lesson_4.urls')), 
+    path('', include('app_billsent.urls')),
+    
 ]
