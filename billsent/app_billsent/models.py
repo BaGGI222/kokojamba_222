dd2e1ce3518e9e2937d12acb72a802ecd11349cd
BodyaQ <vgretchenko77@gmail.com>
2023-08-05 10:26:31 +0300

    test1

--- billsent/app_billsent/models.py
+++ billsent/app_billsent/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
+
 
 class Billsent(models.Model):
     title = models.CharField(verbose_name='Название', max_length=128)
@@ -7,6 +11,15 @@ class Billsent(models.Model):
     auction = models.BooleanField('Торг', help_text='Укажите, если возможен торг')
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description = 'дата создания')
+    def created_date(self):
+        if self.create_at.date() == timezone.now().date():
+            created_time = self.create_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.create_at.strftime('%d.%m.%Y')
 
 def __str__(self):
     return f'Billsent(id={self.id}, title = {self.title}, price = {self.price})'
