43cd00e991f47053d9bbfc41378c260349b4e85e
BodyaQ <vgretchenko77@gmail.com>
2023-08-31 16:45:20 +0300

    test1

--- billsent/app_lesson_4/forms.py
+++ billsent/app_lesson_4/forms.py
@@ -1,7 +1,19 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django contrib.auth.forms import UserCreationForm
 from app_billsent.models import Billsent
 
-class ArticleForm(ModelForm):
+User = get_user_model()
+
+class ExtendedUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['password1'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['password2'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['first_name'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control form-control-lg'
+
     class Meta:
         model = Billsent
         fields = ['pub_date', 'headline', 'content', 'reporter']
